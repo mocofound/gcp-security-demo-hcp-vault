200ca72226c608a8b7d9757b4b762ab0f133ab25
Alex Harness <mocofound@gmail.com>
2022-01-27 15:20:53 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "VAULT_ADDR" {
     value = hcp_vault_cluster.example_vault_cluster.vault_private_endpoint_url
 }
+
+output "HCP_VAULT_ADMIN_TOKEN" {
+    value = hcp_vault_cluster_admin_token.example_vault_admin_token
+}
