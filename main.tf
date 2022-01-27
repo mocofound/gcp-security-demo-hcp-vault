1c6c0ab9b5be495be47ab2d3d1d15295d8bf8757
Alex Harness <mocofound@gmail.com>
2022-01-27 15:03:08 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,17 @@ provider "hcp" {
 }
 
 resource "hcp_vault_cluster" "example_vault_cluster" {
-  hvn_id     = hcp_hvn.example_hvn.hvn_id
+  hvn_id     = hcp_hvn.hvn1.hvn_id
   cluster_id = "hcp-tf-example-vault-cluster"
 }
 
 resource "hcp_vault_cluster_admin_token" "example_vault_admin_token" {
   cluster_id = hcp_vault_cluster.example_vault_cluster.cluster_id
+}
+
+resource "hcp_hvn" "hvn1" {
+  hvn_id         = "main-hvn"
+  cloud_provider = "aws"
+  region         = "us-west-2"
+  cidr_block     = "172.25.17.0/20"
 }
